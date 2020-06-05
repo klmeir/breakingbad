c1d04aee88d0f27ed435074177deae9eec270b77
klmeir <caalmeir@gmail.com>
2020-06-05 00:12:58 -0500

    Add button to consult api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  align-items: center;
+  display: flex;  
+  flex-direction: column;
+  padding-top: 5rem;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35, #0f574e 100%);
+  background-size: 300px;
+  border: 2px solid black;
+  color: #fff;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 2rem;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+`;
 
 function App() {
+
+  const getDataAPI = () => {
+    console.log("consultando...");
+  }
+
   return (
-    <h1>Breaking bad</h1>
+    <Container>
+      <Button
+        onClick={getDataAPI}
+      >
+        Obtener Frase
+      </Button>
+    </Container>    
   )
 }
 
